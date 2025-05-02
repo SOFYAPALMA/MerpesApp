d28c581aff696859a71fc287ce2c096d8a112215
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-05-02 18:31:30 -0500

    creacion de las interfaces (modelos)

---
+++ src/app/Models/Productos.ts
@@ -0,0 +1,9 @@
+export interface Productod{
+    id : number;
+    nombre :  string;
+    descripcion : string;
+    precio :  number;    
+    categoria : string;
+    cantidad :  number;
+    // idcategoria :  number;
+}
