90a66ee691d25de635269ba844b71cb503f8cbb4
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-05-02 19:09:26 -0500

    configuracion de Routes: direccionamiento entre las paginas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductosComponent } from './Pages/productos/productos.component';
+import { EditarProductosComponent } from './Pages/editar-productos/editar-productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/productos', pathMatch: 'full' },
+  { path: 'productos',component: ProductosComponent },
+  { path: 'editar-productos/:id',component: EditarProductosComponent },
+
+];
